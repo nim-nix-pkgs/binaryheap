975183bbcb040178e2504e5dcb5205232583ce65
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:04 -0300

    chore: re index binaryheap

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."binaryheap-master".dir   = "master";
+  inputs."binaryheap-master".owner = "nim-nix-pkgs";
+  inputs."binaryheap-master".ref   = "master";
+  inputs."binaryheap-master".repo  = "binaryheap";
+  inputs."binaryheap-master".type  = "github";
+  inputs."binaryheap-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."binaryheap-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
